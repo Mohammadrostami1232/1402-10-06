4e281cedbc95f80b3deb1da7664e18fa21ccedb8
Mohammadrostami1232 <148641750+Mohammadrostami1232@users.noreply.github.com>
2023-12-29 03:22:43 -0800

    سلام و عرض ادب تمرین چهارشنبه 1402/10/06

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Transactions;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your num");
+            int x = int.Parse(Console.ReadLine());
+            int y = fibo(x-1);
+            Console.WriteLine(y);
+            Console.ReadKey();
+
+        }
+        static int fibo(int x)
+        {
+            if (x <= 1)
+
+                return x;
+
+                return fibo(x -1)+ fibo(x-2);
+            
+
+       
+
+
+
+        }
+    }
+}
